fc6920bcafc47122f60fc35036c5056f4bfe56bd
Julius Diestra <julius.diestra.cabrera@gmail.com>
2021-06-20 16:34:04 +0200

    Adding example-4 and update README file

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Examples using eigen library as submodule.
 
+* **example-01 :** Declare matrix and add values. 
+* **example-02 :** Declare matrix, column vector and row vector. Multiply matrix and vectors. 
+* **example-03 :** Declare matrix, colum vector and row vector. Initialize values with zeros and random values.
+* **example-04 :** Declare matrix, colum vector and row vector. Find transpose and inverse of matrix.
+
 ## How to run examples
 
 1. Initialize Eigen submodule
--- example-03/example_three.cpp
+++ example-03/example_three.cpp
@@ -3,24 +3,19 @@
 
 int main() {
     // Declare vectors and matrices and initialize them directly
-    std::cout << "Initialize Matrices and Vectors" << std::endl;
+    std::cout << "Initialize Matrices and Vectors with zeros" << std::endl;
     Eigen::RowVectorXd vec1 = Eigen::RowVectorXd::Zero(3);      // Declare row vector 1x3
     Eigen::VectorXd vec2 = Eigen::VectorXd::Zero(3);            // Declare column vector 3x1
     Eigen::MatrixXd matrix1 = Eigen::MatrixXd::Zero(3,3);       // Declare matrix 3x3    
+    std::cout << "Row vector: " << std::endl;
     std::cout << vec1 << std::endl;
+    std::cout << "Column vector: " << std::endl;
     std::cout << vec2 << std::endl;
+    std::cout << "Matrix: " << std::endl;
     std::cout << matrix1 << std::endl;
     // Create Random Matrix
     std::cout << "Initialize Matrices with random values" << std::endl;
     Eigen::MatrixXd matrix2 = Eigen::MatrixXd::Random(3,3);      // Declare matrix and initialize with random values.
     std::cout << matrix2 << std::endl;
-    // Matrix Transpose
-    std::cout << "Matrix transpose :" << std::endl;
-    Eigen::MatrixXd matrix2_transpose = matrix2.transpose();
-    std::cout << matrix2_transpose << std::endl;
-    // Matrix Inverse
-    std::cout << "Matrix inverse :" << std::endl;
-    Eigen::MatrixXd matrix2_inverse = matrix2.inverse();
-    std::cout << matrix2_inverse << std::endl;
 }
 
